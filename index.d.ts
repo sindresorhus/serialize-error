a21dcda1aeb554880ca9b9255169f431fbfeec8a
Sindre Sorhus <sindresorhus@gmail.com>
2019-09-23 13:24:31 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -25,7 +25,7 @@ declare const serializeError: {
 	console.log(serializeError(error));
 	//=> {name: 'Error', message: '🦄', stack: 'Error: 🦄\n    at Object.<anonymous> …'}
 
-	// including Date properties
+	// Including Date properties
 	error.time = new Date(0);
 	console.log(serializeError(error));
 	//=> {name: 'Error', message: '🦄', time: 1970-01-01T00:00:00.000Z, stack: 'Error: 🦄\n    at Object.<anonymous> …'}
