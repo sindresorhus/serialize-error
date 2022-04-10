62f9df82b4d155d26946a160ff888d7cef91cb6d
Federico Brigante <me@fregante.com>
2022-04-10 13:05:25 +0800

    Fix type declaration

--- error-constructors.d.ts
+++ error-constructors.d.ts
@@ -1,6 +1,6 @@
 /**
 Map of error constructors to recreate from the serialize `name` property. If the name is not found in this map, the errors will be deserialized as simple `Error` instances.
 */
-const errorConstructors: Map<string, ErrorConstructor>;
+declare const errorConstructors: Map<string, ErrorConstructor>;
 
 export default errorConstructors;
